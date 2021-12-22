5f0f669fd59c85617a91d5146f605452ef31382a
Joshua Anderson <joshuatanderson1@gmail.com>
2021-12-22 16:49:21 -0600

    feat: added contract-sizer dev dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomiclabs/hardhat-waffle";
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
 import '@openzeppelin/hardhat-upgrades';
+import "hardhat-contract-sizer"
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
